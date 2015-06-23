d6f961edae4183ad6962499619afe930c4b4bcef
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2015-06-23 16:25:40 -0400

    Adding step1-get-cmfiles.sh

---
+++ step1-get-cmfiles.sh
@@ -0,0 +1,30 @@
+# EPN, Mon Mar 30 13:31:51 2015
+# 
+# step1-get-cmfiles.sh
+#
+# Obtaining Rfam 12.0 CM files for SSU and LSU rRNA 
+# for testing for possible inclusion in the GPIPE 
+# NCBI prokaryotic genome annotation pipeline.
+#
+# CM files are copied/created in the current working directory
+# and used by the remainder of the scripts in this directory
+# to carry out the tests.
+#
+#############################
+# 1. Download the file Rfam.cm.gz from Rfam 12.0 and gunzip it:
+wget ftp://ftp.ebi.ac.uk/pub/databases/Rfam/12.0/Rfam.cm.gz
+gunzip Rfam.cm.gz
+#
+# 2. Use cmfetch to fetch the SSU and LSU models:
+#
+#  First get all of the SSU and LSU models into one file:
+/usr/local/infernal/1.1.1/bin/cmstat Rfam.cm | grep SSU > ssu-and-lsu.list
+/usr/local/infernal/1.1.1/bin/cmstat Rfam.cm | grep LSU >> ssu-and-lsu.list
+/usr/local/infernal/1.1.1/bin/cmfetch -f Rfam.cm ssu-and-lsu.list > ssu-and-lsu-all.cm 
+#
+#  and make single model files for SSU and LSU archaea and bacteria models
+/usr/local/infernal/1.1.1/bin/cmfetch Rfam.cm SSU_rRNA_archaea  > ssu.arc.cm 
+/usr/local/infernal/1.1.1/bin/cmfetch Rfam.cm SSU_rRNA_bacteria > ssu.bac.cm 
+/usr/local/infernal/1.1.1/bin/cmfetch Rfam.cm LSU_rRNA_archaea  > lsu.arc.cm 
+/usr/local/infernal/1.1.1/bin/cmfetch Rfam.cm LSU_rRNA_bacteria > lsu.bac.cm 
+
